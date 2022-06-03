051bd50a85b06d468836c1e1e39bd0cb8c34bb74
obadaajenah <ajeobada1@gmail.com>
2022-06-04 01:58:21 +0300

    add table refernce_info

---
+++ app/Reference_info.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reference_info extends Model
+{
+    //
+}
---
+++ database/migrations/2022_06_03_221158_create_reference_infos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferenceInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reference_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('car_number')->unique();
+            $table->string('owner');
+            $table->string('type');
+            $table->string('category');
+            $table->date('manufacturing_year');
+            $table->string('engine_number');
+            $table->string('passengers_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reference_infos');
+    }
+}
